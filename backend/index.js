6a98146752374280eb309174e600b8d637726136
Harjot Singh <harjotsingh2k19@gmail.com>
2026-07-02 15:52:12 +0530

    app code change requirement

--- backend/index.js
+++ backend/index.js
@@ -12,11 +12,9 @@ const tripRoutes = require('./routes/trip.routes')
 const apiRouter = express.Router();
 
 // Mount all existing routes onto the API router
-apiRouter.use('/trip', tripRoutes);
-apiRouter.get('/hello', (req, res) => { res.send('Hello World!'); });
+apiRouter.use('/api/trip', tripRoutes);
+apiRouter.get('/api/hello', (req, res) => { res.send('Hello World!'); });
 
-// Mount the apiRouter under the /api prefix
-app.use('/api', apiRouter); // Now your backend listens for /api/trip and /api/hello
 
 app.listen(PORT, ()=>{
     console.log(`Server started at http://localhost:${PORT}`)
